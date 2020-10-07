46007b632cbb75eee26f542818cdeadd6f4b3368
Garrett <gman9mm@live.com>
2020-10-07 16:56:32 -0700

    Page displays all properl. edit, add, new, etc. all functional.
    Co-authored-by: Johnny Duverseau <duverseaujohnny21@gmail.com>

--- BestRestaurant/Controllers/CuisinesController.cs
+++ BestRestaurant/Controllers/CuisinesController.cs
@@ -3,7 +3,6 @@ using BestRestaurant.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BestRestaurant.Controllers
 {
